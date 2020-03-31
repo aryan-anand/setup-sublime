5514bd313b2af32398cf829fdd5058325dfa479f
aryananand42 <35937853+aryananand42@users.noreply.github.com>
2020-04-01 00:29:36 +0530

    Add files via upload

---
+++ c++/code.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  //ctrl+shift+B to run the code for 1st time
+   // only ctrl+B to run 2nd time
+  #ifndef Online_Judge
+	  freopen("input.txt","r",stdin);
+	  freopen("output.txt","w",stdout);
+  #endif
+
+	//Write code here
+
+	  
+	  int x; cin>>x;
+
+	  cout<<x+10;
+	  
+
+	
+	
+	
+
+}
